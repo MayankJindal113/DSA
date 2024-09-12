e8e4baef3fcf8bdc36c4f6ed2d03131445ccee41
MayankJindal113 <mjindal1_be21@thapar.edu>
2024-09-12 23:54:26 +0530

    LeetCode Updated

--- Basics/str_cmp.cpp
+++ Basics/str_cmp.cpp
@@ -1,4 +1,4 @@
-// Compare the i th and jth position of the two strings 
+// Compare the ith and jth position of the two strings 
 // And print whether that part of the strings is same or not ?
 
 //     for(int i=0;i<6;i++ )
@@ -31,23 +31,50 @@
 
 
 
-
-
 // Good String :
 // The distance between two adjacent characters should not be more than one
 
 
-
 #include<iostream>
+#include <string>
+#include<vector>
 using namespace std; 
 
 
+string encode(vector<string>& strs) {
+        // In encoding, pre-concatenate each element with its length
+        // Then concatenate all the elements
+        string s="";
+        for (string i : strs)
+        {
+            int n = i.length();
+            string c= "";
+            c+= n;
+            c+= i;
+            s+= c;
+        }
+        return s;
+}
+
+ 
 int main()
 {
-    string (s1);
-    string (s2);
-    s2="Mayank";
-    s1="Madhav";
+    // string (s1);
+    // string (s2);
+    // s2="Mayank";
+    // s1="Madhav";
+    // vector<string> a ={"Hi", "my", "nam eh", "this i"};
+    // string ans = encode (a);
+    // int len = ans.length();int i =0;
+    // while (i < len)
+    // {
+    //     cout << ans[i];i++;
+    // }
+    // string a ="This is a queote";
+    // cout << a.length()<< a.size();
 
-    
+    int a = 1450;
+    char b = a;
+    int c = b;
+    cout << a<<endl << b<<endl << c<<endl;
 }
--- Basics/string_size.cpp
+++ Basics/string_size.cpp
@@ -1,15 +1,27 @@
-// Remember there is a null stored in at the end of a string always
+// Remember there is a null stored in at the end of a character array always but not a string (C++)
+
+//  stoi, to_string functions
+//  To convert stoi : string to int
+//  int to_string
 
 #include<iostream>
+#include <string>
 using namespace std;
 
 int main()
 {
-    char a[]="mayank";
-    char nums[]="123";
-    int b = nums [0] - '0'; // Typecasting by subtracting the ASCII value of the first character ie 0
-    cout <<  b;
+    // char a[]="mayank"; // the length of this char array is 6 + 1 bcz of the null ptr at the end (npos)
+    // // Its useful to identify when the end of a char array has been reached 
+    // char nums[]="123";
+    // int b = nums [0] - '0'; // Typecasting by subtracting the ASCII value of the first character ie 0
+    // cout <<  b;
     // int val = int(nums[0])+int(nums[1]); //typecasting
     // cout << val;
     // cout << sizeof(a)/sizeof(a[0]-1);
+
+    int br = 12;
+    string c = to_string(br);
+    int d = stoi (c);
+    cout << c<<endl<< d;
+
 }
